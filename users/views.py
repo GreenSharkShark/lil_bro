e9fa04a8d6a3d0d211c3fe10f171af313688937a
Zhorik <zhorikzeniuk@gmail.com>
2024-10-08 09:18:17 +0200

    added registration and authentication

--- users/views.py
+++ users/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.contrib.auth.views import LoginView
+from django.http import HttpResponse
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+from .forms import UserRegisterForm
+from .models import User
 
-# Create your views here.
+
+class UserRegisterView(CreateView):
+    model = User
+    form_class = UserRegisterForm
+    template_name = 'users/register_and_login.html'
+    success_url = reverse_lazy('lil_bro:secret_create')
+    
+    def form_valid(self, form) -> HttpResponse:
+        response =  super().form_valid(form)
+        user = form.save()
+        login(self.request, user)
+        return response
+    
+    
+class UserLoginView(LoginView):
+    template_name = 'users/register_and_login.html'
+    redirect_authenticated_user = True
+    next_page = reverse_lazy('lil_bro:secret_create')
